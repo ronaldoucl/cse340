fd845a40dcd0bc5d68fdb1b1d3b4f882dbff6a40
Ronaldo Campos <cam23008@byui.edu>
2025-03-25 11:45:01 -0600

    changes

--- controllers/intentionalErrorController.js
+++ controllers/intentionalErrorController.js
@@ -3,8 +3,7 @@ const intentionalErrorController = {};
 intentionalErrorController.causeError = async function(req, res, next) {
     console.log("Causing an error...");
     let aNumber = 1/0;
-    throw new Error("This is an intentional error.");
-    // The render templates expect data that is not being provided. This will also cause an exception.
+    throw new Error("Oh no! There was a crash. Maybe try a different route?");
     res.render("./", {
         title: "Intentional Error",
     })
